1a04c792f0130794aeb314d08ac4b96ecba7f004
Eswara Poojari <eswara.poojari@gmail.com>
2018-12-29 19:19:07 +0530

    Bundle of files committed

--- springboot1.4.2/src/main/java/com/practice/springbootstarter/CourseApiApp.java
+++ springboot1.4.2/src/main/java/com/practice/springbootstarter/CourseApiApp.java
@@ -1,23 +1,33 @@
 package com.practice.springbootstarter;
 
+import java.math.BigDecimal;
+
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.Environment;
+
+import com.practice.springbootstarter.components.MyProperties;
+import com.practice.springbootstarter.components.RandomProperties;
+import com.practice.springbootstarter.profile.InventoryService;
+import com.practice.springbootstarter.profile.YmlBean;
 
 @SpringBootApplication
 public class CourseApiApp {
-
+	//private static final Logger logger = Logger.getLogger(CourseApiApp.class.getName());
 	public static void main(String[] args) {
-		
-		ConfigurableApplicationContext appContext = SpringApplication.run(CourseApiApp.class, args);
-		
-		Environment environment = appContext.getBean(Environment.class);
-		String[] activeProfiles = environment.getActiveProfiles();
-		for(final String profile:activeProfiles){
-			System.out.println("Profile: "+profile);
-		}
-
+		//logger.warn("--start--");
+		ConfigurableApplicationContext context = SpringApplication.run(CourseApiApp.class, args);
+		MyProperties bean = context.getBean(MyProperties.class);
+		System.out.println(bean);
+		RandomProperties randomBean = context.getBean(RandomProperties.class);
+		System.out.println(randomBean);
+		InventoryService inventory = context.getBean(InventoryService.class);
+		inventory.service("Item-1", 2, BigDecimal.valueOf(20.5));
+		YmlBean ymlBean = context.getBean(YmlBean.class);
+		System.out.println(ymlBean);
+		//logger.error("--end--");
+		System.out.println(System.getProperty("user.dir"));
 	}
 
 }
